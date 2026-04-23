50439ba1c03cbfee23af3b55eb6632d82fe16887
noobird <jinghuaswsx@gmail.com>
2026-04-23 13:51:19 +0800

    fix: move video translate menu to bottom

--- tests/test_av_sync_menu_routes.py
+++ tests/test_av_sync_menu_routes.py
@@ -11,16 +11,33 @@ def test_av_sync_menu_page_renders_shared_workbench(authed_client_no_db):
     assert 'href="/video-translate-av-sync"' in html
 
 
-def test_dashboard_sidebar_places_av_sync_at_sidebar_bottom(authed_client_no_db):
+def test_dashboard_sidebar_places_video_translate_at_sidebar_bottom(authed_client_no_db):
+    resp = authed_client_no_db.get("/video-translate-av-sync")
+    assert resp.status_code == 200
+    html = resp.get_data(as_text=True)
+    nav_html = html[html.index('<nav class="sidebar-nav">'):html.index("</nav>")]
+
+    lab_idx = nav_html.index('href="/translate-lab"')
+    av_sync_idx = nav_html.index('href="/video-translate-av-sync"')
+    video_translate_idx = nav_html.index('href="/"')
+    video_translate_anchor_idx = nav_html.rfind('<a href="/"')
+    last_anchor_idx = nav_html.rfind("<a ")
+
+    assert lab_idx < av_sync_idx < video_translate_idx
+    assert video_translate_anchor_idx == last_anchor_idx
+
+
+def test_dashboard_sidebar_av_sync_uses_icon_instead_of_av_text(authed_client_no_db):
     resp = authed_client_no_db.get("/video-translate-av-sync")
     assert resp.status_code == 200
     html = resp.get_data(as_text=True)
 
-    lab_idx = html.index('href="/translate-lab"')
-    av_sync_idx = html.index('href="/video-translate-av-sync"')
-    nav_end_idx = html.index("</nav>")
-
-    assert lab_idx < av_sync_idx < nav_end_idx
+    match = re.search(
+        r'<a href="/video-translate-av-sync"[^>]*>\s*<span class="nav-icon">([^<]+)</span>',
+        html,
+    )
+    assert match
+    assert match.group(1).strip() != "AV"
 
 
 def test_dashboard_sidebar_marks_av_sync_entry_active(authed_client_no_db):
