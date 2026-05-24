ceaa1c7d4f932f3cb33b87d17ea1ebd4da19d60b
noobird <jinghuaswsx@gmail.com>
2026-05-24 18:01:45 +0800

    fix: remove copy-pasted line numbers from migration sql file

--- db/migrations/2026_05_24_add_shopify_product_id_to_ai_listing.sql
+++ db/migrations/2026_05_24_add_shopify_product_id_to_ai_listing.sql
@@ -1,4 +1,4 @@
 -- Migration to add shopify_product_id and shopify_published_at to ai_listing_tasks
-3: ALTER TABLE `ai_listing_tasks` 
-4: ADD COLUMN `shopify_product_id` VARCHAR(64) DEFAULT NULL COMMENT 'Shopify 生成的商品 ID' AFTER `error_message`,
-5: ADD COLUMN `shopify_published_at` DATETIME DEFAULT NULL COMMENT 'Shopify 上架发布时间' AFTER `shopify_product_id`;
+ALTER TABLE `ai_listing_tasks` 
+ADD COLUMN `shopify_product_id` VARCHAR(64) DEFAULT NULL COMMENT 'Shopify 生成的商品 ID' AFTER `error_message`,
+ADD COLUMN `shopify_published_at` DATETIME DEFAULT NULL COMMENT 'Shopify 上架发布时间' AFTER `shopify_product_id`;
